fdb6a6204428654194bfeca07433c6e720812212
itdev-lenovo <it1devops@gmail.com>
2020-04-08 12:10:45 +0530

    files added

---
+++ First App and JavaScript-ES6 Basics/First App and JavaScript-ES6 Basics.js
@@ -0,0 +1,14 @@
+// Task 1
+// Write the program using the variables and strings in ES6
+****************
+// Task 2
+/*Write the program using the following operators in ES6 
+a.	Ternary operator
+b.	Binary operator
+c.	Unary operator
+*/
+****************
+// Task 3
+/*Write the program using the conditional statements and loops in ES6,
+Write the program using Arrays, JavaScript objects and Functions in ES6
+*/
---
+++ Intro to JavaScript/Intro to JavaScriptl.js
@@ -0,0 +1,13 @@
+// Task 1
+// Write the program for adding and subtracting two numbers using JavaScript
+****************
+// Task 2
+// Write the program for creating and declare the JavaScript variable and using String in JavaScript
+****************
+// Task 3
+/*Write the program based on the following operators in JavaScript
+a.	Logical operator
+b.	Comparison operators,
+
+Write the program to create an array and fetch the elements from the array and create the program using loops in JavaScript
+*/
